353c2a29c8e1a51f9a93c1d066c468aaaaf7b3be
Ali Fertah <85651433+alifertah@users.noreply.github.com>
2022-06-11 16:51:53 +0100

    init project

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import About from "."
+
+function MyApp() {
+  return <About />
+}
+
+export default MyApp
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import styl
+
+function About() {
+  return <div>About</div>
+}
+
+export default About
