2fd90fde0703b6e7e2cdaf8256f34174f61b33c3
Ali Fertah <85651433+alifertah@users.noreply.github.com>
2022-06-18 08:48:56 +0100

    changing the news to moroccan news

--- pages/feed/[id].js
+++ pages/feed/[id].js
@@ -54,7 +54,7 @@ export const getServerSideProps = async pageContext => {
     }
 
     const apiResponse = await fetch(
-        `https://newsapi.org/v2/top-headlines?country=us&pageSize=5&page=${pageNumber}`,
+        `https://newsapi.org/v2/top-headlines?country=ma&pageSize=5&page=${pageNumber}`,
         {
             headers: {
                 Authorization: `Bearer ${process.env.NEXT_PUBLIC_NEWS_KEY}`,
